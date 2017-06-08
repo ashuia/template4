5a8c0f80c4b201b94dc6a969f7602ea5f0ad67a9
ashui <ashui@ashuihouse.cn>
2017-06-08 13:52:49 +0800

    4.2
    4.2

--- main/student.js
+++ main/student.js
@@ -1,3 +1,8 @@
 let Person = require('./person')
 // Write your code here
 module.exports = Student
+function Student(name,age,klass){
+  this.Person=Person;
+  this.Person(name,age);
+  this klass=klass;
+}
