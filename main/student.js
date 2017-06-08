23157e00e8575caf51af47aca553224f1fa42852
ashui <ashui@ashuihouse.cn>
2017-06-08 13:57:30 +0800

    4.5
    45.

--- main/student.js
+++ main/student.js
@@ -5,7 +5,7 @@ function Student(name,age,klass){
   this.Person=Person;
   this.Person(name,age);
   this klass=klass;
-  function introduce{
+  this introduce=function{{
       var str;
       str="My name is "+this.name+". I am "+this.age+" years old."+"I am a Student. I am at Class"+klass;
       return str;
