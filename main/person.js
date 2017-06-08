83bb518970cc37c9d9ded4f84cdc2927324a7786
ashui <ashui@ashuihouse.cn>
2017-06-08 13:58:21 +0800

    4.5
    4.5

--- main/person.js
+++ main/person.js
@@ -3,7 +3,7 @@ module.exports = Person
 function Person(name,age){
     this.name=name,
     this.age=age;
-    this introduce=function{
+    this.introduce=function{
       var str;
       str="My name is "+this.name+". I am "+this.age+" years old.";
       return str;
