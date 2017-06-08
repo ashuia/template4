912e0646a70dfcfd1e3255f8dc71494d2846323b
ashui <ashui@ashuihouse.cn>
2017-06-08 14:01:51 +0800

    4.7
    4.7

--- main/student.js
+++ main/student.js
@@ -4,7 +4,7 @@ module.exports = Student
 function Student(name,age,klass){
   this.Person=Person;
   this.Person(name,age);
-  this klass=klass;
+  this.klass=klass;
   this.introduce=function(){
       var str;
       str="My name is "+this.name+". I am "+this.age+" years old."+"I am a Student. I am at Class"+klass;
